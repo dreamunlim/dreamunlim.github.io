85574b9e7e71f02eda9c94f913820e8d1dc51605
tolerance1 <tolerance1@users.noreply.github.com>
2020-01-04 22:06:14 +0200

    Implemented player movement logic

--- js/player.js
+++ js/player.js
@@ -1,56 +1,125 @@
 'use strict'
 
 class Player extends GameObject {
-    constructor () {
+    constructor() {
         super();
-        
+
+        this.lanes = gameStateMachine.stack[gameStateMachine.stack.length - 1].lanes; // offscreen lanes at sides
+        this.currLane = 3;
+        this.targLane = 3;
+        this.numLanes = this.lanes.length;
+
+        this.teleport = false;
+        this.animate = false;
+        this.time = FPS; // in frames
+        this.dist = this.lanes[1] - this.lanes[0]; // constant
+        // this.accel = 2 * this.dist / (this.time * this.time); // a = 2d/t^2 when initial velocity == 0
     }
 
     initObject(initData) {
         super.initObject(initData);
+
+        this.accel = 2 * (this.dist * this.velocity.x * this.time) / (this.time * this.time);
+        this.velocity.x = 0;
     }
 
     updateObject() {
-        //reset velocity
-
-
-        if (inputHandler.leftPressed) {
-            this.position.x -= this.velocity.x - this.acceleration.x;
-        }
-        if (inputHandler.rightPressed) {
-            this.position.x += this.velocity.x + this.acceleration.x;
-        }
-        if (inputHandler.upPressed) {
-            this.position.y -= this.velocity.y - this.acceleration.x;
-        }
-        if (inputHandler.downPressed) {
-            this.position.y += this.velocity.y + this.acceleration.x;
-        }
-
-        if (inputHandler.mouseLeftPressed) {
-            var to;
-
-            if (window.innerWidth < window.innerHeight) {
-                to = new Vector2D(inputHandler.mEvent.clientX/scale, inputHandler.mEvent.clientY/scale);
-            } else {
-                to = new Vector2D(inputHandler.mEvent.offsetX, inputHandler.mEvent.offsetY);
-            }
-
-            this.position = to;
-
-            // be sure 'player' has indeed had time to update itself  
-            inputHandler.mouseLeftPressed = false;
-        }
-
         super.updateObject();
+
+        this.currLane = this.targLane;
+
+        // decide velocity and acceleration reset
+        if (this.animate) {
+            // snap player to targetX in this frame to avoid currX > targetX in next frame
+            var snapLeft = (this.acceleration.x < 0) && (this.position.x <= this.lanes[this.targLane]);
+            var snapRight = (this.acceleration.x > 0) && (this.position.x >= this.lanes[this.targLane]);
+
+            if (snapLeft || snapRight) {
+                this.position.x = this.lanes[this.targLane];
+                this.velocity.x = 0;
+                this.acceleration.x = 0;
+                this.animate = false;
+
+                // if it was a teleport snap
+                if (this.teleport && snapLeft) {
+                    this.currLane = this.targLane = 5;
+                    this.position.x = this.lanes[this.targLane];
+                    this.teleport = false;
+                } else if (this.teleport && snapRight) {
+                    this.currLane = this.targLane = 1;
+                    this.position.x = this.lanes[this.targLane];
+                    this.teleport = false;
+                }
+            }
+        }
+
+        // keyboard
+        // disallow 'targLane' change during animation to avoid lane fly-throughs, especially at edges
+        if (inputHandler.leftPressed && !this.animate) {
+            this.targLane = mod(--this.targLane, this.numLanes);
+        }
+        if (inputHandler.rightPressed && !this.animate) {
+            this.targLane = (++this.targLane) % this.numLanes;
+        }
+
+        // mouse
+        if (inputHandler.mouseLeftPressed && !this.animate) {
+            if (inputHandler.mEvent.clientX < window.innerWidth / 2) {
+                this.targLane = mod(--this.targLane, this.numLanes);
+            }
+            else {
+                this.targLane = (++this.targLane) % this.numLanes;
+            }
+        }
+
+        // decide movement direction
+        if (this.targLane == 0 && this.currLane == 1) {
+            // left edge teleport
+            this.acceleration.x = -this.accel;
+            this.animate = true;
+            this.teleport = true;
+
+        } else if (this.targLane == 6 && this.currLane == 5) {
+            // right edge teleport
+            this.acceleration.x = this.accel;
+            this.animate = true;
+            this.teleport = true;
+
+        } else if (this.targLane < this.currLane) {
+            this.acceleration.x = -this.accel;
+            this.animate = true;
+
+        } else if (this.targLane > this.currLane) {
+            this.acceleration.x = this.accel;
+            this.animate = true;
+
+        }
+
+
+        // be sure 'player' has indeed had time to update itself
+        inputHandler.leftPressed = false;
+        inputHandler.rightPressed = false;
+        inputHandler.mouseLeftPressed = false;
     }
 
 
     drawObject() {
-        this.currentFrame = Math.floor((time / this.animSpeed) % this.numFrames);
+        if (this.teleport) {
+            // draw one sprite offscreen
+            var x = this.position.x;
+
+            if (this.acceleration.x < 0) {
+                this.position.x = this.position.x + 5 * this.dist;
+            }
+            else {
+                this.position.x = this.position.x - 5 * this.dist;
+            }
+
+            super.drawObject();
+
+            this.position.x = x;
+        }
 
         super.drawObject();
     }
 }
-
-console.log('PLAYER.JS');
