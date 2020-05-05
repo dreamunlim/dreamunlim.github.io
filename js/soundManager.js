752e4e79eeb73c2ce017b53377cf9bffa7839a66
tolerance1 <tolerance1@users.noreply.github.com>
2020-05-05 16:34:11 +0300

    Implemented sound manager

--- js/soundManager.js
+++ js/soundManager.js
@@ -1,6 +1,52 @@
 'use strict'
 
+const audioCtx = new AudioContext();
+
 class SoundManager {
     constructor() {
-
+        this.soundMap = new Map();
     }
+
+    async storeSound(soundID, filePath) {
+        // var sound = new Audio();
+        // sound.src = filePath;
+
+        // this.soundMap.set(soundID, sound);
+
+        const response = await fetch(filePath);
+        const arrayBuffer = await response.arrayBuffer();
+        const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+
+        this.soundMap.set(soundID, audioBuffer);
+    }
+
+    async playSound(soundID) {
+        //     var sound = this.soundMap.get(soundID);
+
+        //     sound.load(); // reset playback to the beginning
+        //     var playPromise = sound.play();
+
+        //     if (playPromise !== undefined) {
+        //         playPromise.then(_ => {
+        //           // Audio playback started
+        //         })
+        //         .catch(error => {
+        //           // Audio playback failed
+        //         });
+        //       }
+        // }
+
+        const track = await this.soundMap.get(soundID);
+
+        // check if context is in suspended state (autoplay policy)
+        if (audioCtx.state === 'suspended') {
+            audioCtx.resume();
+        }
+
+        const trackSource = audioCtx.createBufferSource();
+        trackSource.buffer = track;
+        trackSource.connect(audioCtx.destination);
+
+        trackSource.start();
+    }
+}
