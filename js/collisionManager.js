67767e3f562a34d489efc370ded287643d419d94
tolerance1 <tolerance1@users.noreply.github.com>
2020-05-05 16:32:14 +0300

    Implemented collision manager

--- js/collisionManager.js
+++ js/collisionManager.js
@@ -4,3 +4,30 @@ class CollisionManager {
     constructor() {
 
     }
+
+    playerEnemyCollision(player, enemy) {
+        var playerCircleCentreVec = player.position.add(player.collisionCircle);
+        var enemyCircleCentreVec = enemy.position.add(enemy.collisionCircle);
+
+        var distanceVec = enemyCircleCentreVec.subtract(playerCircleCentreVec);
+
+        if ((player.collisionCircle.radius + enemy.collisionCircle.radius) > distanceVec.vecLength()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    mouseUIButtonCollision(mEvent, button) {
+        if (inputHandler.mouseLeftPressed) {
+            if (mEvent.clientX > button.position.x &&
+                mEvent.clientX < button.position.x + button.sWidth &&
+                mEvent.clientY > button.position.y &&
+                mEvent.clientY < button.position.y + button.dHeight) {
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
