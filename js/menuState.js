fbbae841d6b7d53c5877681b3ec9765e3a52ab3a
tolerance1 <tolerance1@users.noreply.github.com>
2020-06-19 15:31:30 +0300

    Implemented updateTopScore() function

--- js/menuState.js
+++ js/menuState.js
@@ -4,7 +4,7 @@ class MenuState extends GameState {
     constructor() {
         super();
 
-        this.topScore = [0, 23, 255];
+        this.topScore = [[0,0], [0,0], [0,0]]; // score-time pairs
 
         this.titleFont = "68px Bebas Neue";
         this.scoreFont = "35px Orbitron";
@@ -40,16 +40,28 @@ class MenuState extends GameState {
         var x = 20;
         var y = 420;
         var spacing = 35;
-        drawText("TOP SCORE", x + 2, y, this.scoreFont, "start", "coral");
-        drawText("TOP SCORE", x, y, this.scoreFont, "start", "darkgoldenrod");
-
         var charWidth = ctx.measureText("A").width;
         
+        ctx.shadowOffsetX = 2;
+        ctx.shadowOffsetY = 0;
+        ctx.shadowColor = "coral";
+        
+        drawText("TOP SCORE", x, y, this.scoreFont, "start", "darkgoldenrod");
+
         for (var i = 0; i != this.topScore.length; ++i) {
             y = y + spacing;
             drawText((i+1), x + charWidth, y, this.scoreFont, "right", "darkgoldenrod");
-            drawText(this.topScore[i], x + charWidth * 2, y, this.scoreFont, "start", "darkgoldenrod");
+            drawText(this.topScore[i][0], x + charWidth * 2, y, this.scoreFont, "start", "darkgoldenrod");
         }
+
+        // turn off shadow
+        ctx.shadowColor = "rgba(0,0,0,0)";
+    }
+
+    updateTopScore(score, time) {
+        this.topScore.push([score, time]);
+        insertionSort(this.topScore);
+        this.topScore.pop(); // limit array length to three pairs
     }
 
     onEnter() {
@@ -72,10 +84,12 @@ class MenuState extends GameState {
         // "this" points to "funcPointersMap" from here
         // can't do "this.topScore[0] === 0"
         
-        if(button.state.topScore[0] === 0) {
+        if(button.state.topScore[0][0] === 0) {
             button.hintMessage = "Zero Score";
             return;
         }
+
+        button.hintMessage = "Not Ready";
     }
 
     overridePic(button) {
