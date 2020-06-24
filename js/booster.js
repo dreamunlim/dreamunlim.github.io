7e8329618164e48148fe444f945b271d0fb14ed0
tolerance1 <tolerance1@users.noreply.github.com>
2020-06-24 20:05:02 +0300

    Implemented booster spawn logic

--- js/booster.js
+++ js/booster.js
@@ -3,7 +3,13 @@
 class Booster extends Enemy {
     constructor () {
         super();
+
+        // booster owner
+        this.state = gameStateMachine.stack[1];
+        this.state.boosterObject = this;
         
+        this.spawned = false;
+        this.spawnTimerValue = 2; // in sec
     }
 
     initObject(initData) {
@@ -11,11 +17,24 @@ class Booster extends Enemy {
     }
 
     updateObject() {
+        // check if time to spawn
+        if (this.state.timerObject.timerCurrentValue == this.spawnTimerValue) {
+            this.spawned = true;
+        }
+        
+        if(! this.spawned) {
+            return;
+        }
+
         super.updateObject();
     }
 
 
     drawObject() {
+        if(! this.spawned) {
+            return;
+        }
+
         super.drawObject();
     }
 }
