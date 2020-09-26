bacbf5d71e91622bf20cec40ef25043c5e2ef18f
tolerance1 <tolerance1@users.noreply.github.com>
2020-09-26 23:49:25 +0300

    Refactored PlayBackground class

--- js/playBackground.js
+++ js/playBackground.js
@@ -6,10 +6,11 @@ class PlayBackground extends GameObject {
 
         this.t1 = 0;
         this.t2 = 0;
-        this.seconds = 5;
+        this.rumblePeriod = 5; // in sec
 
-        this.intialPos;
-        this.rumblePattern;
+        this.intialPos = null;
+        this.returnVec = null;
+        this.rumblePattern = [[-1,1], [1,1], [1,-1], [-1,-1]];
         this.rumble = false;
         this.currStep = 0;
     }
@@ -18,7 +19,6 @@ class PlayBackground extends GameObject {
         super.initObject(initData);
 
         this.intialPos = new Vector2D(this.position.x, this.position.y);
-        this.rumblePattern = [[-1,1], [1,1], [1,-1], [-1,-1]];
 
         this.initRumblePattern();
 
@@ -34,38 +34,41 @@ class PlayBackground extends GameObject {
     initRumblePattern() {
         for (var i = 0; i < this.rumblePattern.length; ++i) {
             // distance range. max is 10 to stay within +-20px boundaries
-            var scale = random(5,10);
+            var scale = random(5, 10);
 
-            this.rumblePattern[i][0] = this.rumblePattern[i][0] * scale; // x
-            this.rumblePattern[i][1] = this.rumblePattern[i][1] * scale; // y
+            // substitute rumble pattern pair with a vector
+            this.rumblePattern[i] = new Vector2D(this.rumblePattern[i][0] * scale, this.rumblePattern[i][1] * scale);
         }
     }
 
     updateObject() {
-        this.t2 = Math.floor(time / (this.seconds * 1000));
+        this.t2 = Math.floor(time / (this.rumblePeriod * 1000));
 
         // rumble once every n seconds
         // side effect with two rumbles in a row occurs
         if (this.t1 < this.t2) {
             // console.log(this.t1,this.t2);
             this.rumble = true;
-            this.seconds = random(5, 6); // define next call period
+            this.rumblePeriod = random(5, 6); // define next call period
         }
 
-        // avoid t2 < t1, which occurs often on 'seconds' increased 
+        // avoid t2 < t1, which occurs often on rumblePeriod increased 
         this.t1 = this.t2;
 
 
         if (this.rumble) {
             // return to initial position
             if (this.currStep == this.rumblePattern.length) {
-                var final = this.intialPos.subtract(this.position);
-                this.position = this.position.add(final);
+                if (this.returnVec == null) {
+                    this.returnVec = this.intialPos.subtract(this.position);
+                }
+
+                this.position = this.position.add(this.returnVec);
                 this.currStep = 0;
                 this.rumble = false;
 
             } else {
-                var vec = new Vector2D(this.rumblePattern[this.currStep][0], this.rumblePattern[this.currStep][1]);
+                var vec = this.rumblePattern[this.currStep];
                 this.position = this.position.add(vec);
                 ++this.currStep;
             }
