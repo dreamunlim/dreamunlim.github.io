1e2468f0450c53a8f65a87286f68c0b6c78ec4f4
tolerance1 <tolerance1@users.noreply.github.com>
2020-09-26 17:41:13 +0300

    Amended Star object animation logic

--- js/star.js
+++ js/star.js
@@ -7,10 +7,16 @@ class Star extends Enemy {
         this.spawned = false;
         this.spawnPeriod = 2; // in sec
         this.totalRows = 4;
+
+        this.t1 = 0;
+        this.t2 = 0;
     }
 
     initObject(initData) {
         super.initObject(initData);
+
+        // set animation start frame
+        this.currentFrame = random(0, this.numFrames - 1);
     }
 
     updateObject() {
@@ -24,7 +30,7 @@ class Star extends Enemy {
         }
 
         super.updateObject();
-        super.animateFrame();
+        this.animateFrame();
     }
 
 
@@ -34,5 +40,14 @@ class Star extends Enemy {
         }
 
         super.drawObject();
+    }
+
+    animateFrame() {
+        this.t2 = Math.floor(time / this.animSpeed);
+
+        if (this.t1 < this.t2) {
+            this.currentFrame = (++this.currentFrame) % this.numFrames;
+            this.t1 = this.t2;
+        }
     }
 }
