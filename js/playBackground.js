71826f51d4683ad3ce18cf48fd5c5d21c86cabd8
tolerance1 <tolerance1@users.noreply.github.com>
2021-09-16 00:00:13 +0300

    Implemented play background transition effect

--- js/playBackground.js
+++ js/playBackground.js
@@ -1,5 +1,40 @@
 'use strict'
 
+class PendingPlayBackground {
+    static previousTextureID = ""; // keep value on destruction 
+
+    constructor(that) {
+        this.playBackground = that;
+
+        this.transitionSteps = FPS;
+        this.stepMagnitude = 1 / this.transitionSteps;
+        this.canvasAlphaValue = 0;
+        this.transition = false;
+        this.newTextureID = "";
+    }
+
+    drawWithAlpha(alpha) {
+        ctx.save();
+        ctx.globalAlpha = alpha;
+        textureManager.drawTexture(this.newTextureID, this.playBackground.currentFrame, this.playBackground.currentRow,
+            this.playBackground.sWidth, this.playBackground.sHeight, this.playBackground.position.x, this.playBackground.position.y,
+            this.playBackground.dWidth, this.playBackground.dHeight);
+        ctx.restore();
+    }
+
+    drawObject() {
+        this.drawWithAlpha(this.canvasAlphaValue);
+
+        this.canvasAlphaValue += this.stepMagnitude;
+        this.transitionSteps -= 1;
+
+        if (this.transitionSteps == 0) {
+            this.transition = false;
+            this.playBackground.textureID = this.newTextureID;
+        }
+    }
+}
+
 class PlayBackground extends GameObject {
     constructor() {
         super();
@@ -15,15 +50,26 @@ class PlayBackground extends GameObject {
         this.currStep = 0;
 
         this.textureIDs = ["play-background-1", "play-background-2", "play-background-3"];
+
+        this.pendingPlayBackground = new PendingPlayBackground(this);
     }
 
     initObject(initData) {
         super.initObject(initData);
 
-        this.textureID =  this.textureIDs[random(0, this.textureIDs.length - 1)];
         this.intialPos = new Vector2D(this.position.x, this.position.y);
-
         this.initRumblePattern();
+
+        // define textureID
+        if (this.pendingPlayBackground.constructor.previousTextureID.length == 0) {
+            this.textureID = this.textureIDs[random(0, this.textureIDs.length - 1)];
+            this.pendingPlayBackground.constructor.previousTextureID = this.textureID;
+        } else {
+            this.pendingPlayBackground.transition = true;
+            this.pendingPlayBackground.newTextureID = this.textureIDs[random(0, this.textureIDs.length - 1)];
+            this.textureID = this.pendingPlayBackground.constructor.previousTextureID;
+            this.pendingPlayBackground.constructor.previousTextureID = this.pendingPlayBackground.newTextureID;
+        }
 
         // remove unused properties
         delete this.lanes;
@@ -78,8 +124,11 @@ class PlayBackground extends GameObject {
         }
     }
 
-
     drawObject() {
         super.drawObject();
+
+        if (this.pendingPlayBackground.transition) {
+            this.pendingPlayBackground.drawObject();
+        }
     }
 }
