c270cd3b8113555fac8daf6afd3f82c01016e62e
tolerance1 <tolerance1@users.noreply.github.com>
2024-09-05 16:54:09 +0300

    Handled a gameplay micro-freeze on the diamond sprite being drawn for the first time

--- js/diamond.js
+++ js/diamond.js
@@ -1,4 +1,5 @@
 import { Star } from "./star.js";
+import { GameObject } from "./gameObject.js";
 import { soundManager } from "./soundManager.js";
 
 class Diamond extends Star {
@@ -9,26 +10,30 @@ class Diamond extends Star {
 
         this.spawnPeriod = 3; // in sec
         this.totalRows = 6;
-
-        this.enemyClass = Object.getPrototypeOf(Object.getPrototypeOf(this));
     }
 
-    updateObject() {
-        // check if time to spawn
-        var totalSecondsPassed = Math.floor(this.state.timerObject.totalTimePassed / 1000);
+    initObject(initData) {
+        super.initObject(initData);
+
+        // trigger image decode in advance to avoid a gameplay micro-freeze on the sprite first draw
+        this.triggerImageDecode();
+    }
+
+    triggerImageDecode() {
+        const [x, y] = [this.position.x, this.position.y];
+        [this.position.x, this.position.y] = [0, 0];
+        GameObject.prototype.drawObject.call(this);
+        [this.position.x, this.position.y] = [x, y];
+    }
+
+    spawnObject() {
+        let totalSecondsPassed = Math.floor(this.state.timerObject.totalTimePassed / 1000);
         if (totalSecondsPassed != 0 &&
             totalSecondsPassed % this.spawnPeriod == 0) {
             this.spawned = true;
         }
-
-        if (! this.spawned) {
-            return;
-        }
-
-        this.enemyClass.updateObject.call(this);
-        super.animateFrame();
     }
-
+    
     respondToCollision() {
         this.state.scoreObject.score += this.pointsToAdd * this.state.pointsMultiplier.activeMultiplier;
         this.respawnAfterDraw = true;
